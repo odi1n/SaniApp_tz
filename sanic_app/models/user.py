e5f563f2f0a77423929652f15575b84cba2c49ba
odi1n <sashf1999@mail.ru>
2022-10-25 13:03:57 +0300

    Поправил модель пользователя

--- sanic_app/models/user.py
+++ sanic_app/models/user.py
@@ -3,7 +3,7 @@ from tortoise import Model, fields
 
 class User(Model):
     id = fields.IntField(pk=True)
-    login = fields.CharField(max_length=50, unique=True)
+    username = fields.CharField(max_length=50, unique=True)
     hashed_password = fields.CharField(null=True, max_length=255)
     is_active = fields.BooleanField(null=False, default=False)
     confirmation = fields.UUIDField(null=True)
@@ -12,4 +12,7 @@ class User(Model):
         table: str = "user"
 
     def __str__(self):
-        return f"User {self.id}: {self.login}"
+        return f"User {self.id}: {self.username}"
+
+    def to_dict(self):
+        return {"user_id": self.id, "username": self.username}
