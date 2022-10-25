aad7199b40fe90a21020b1b885535e0aa8b236f4
odi1n <sashf1999@mail.ru>
2022-10-26 00:20:19 +0300

    Поправил модель пользователя

--- sanic_app/models/user.py
+++ sanic_app/models/user.py
@@ -1,12 +1,14 @@
+from uuid import uuid4
+
 from tortoise import Model, fields
 
 
 class User(Model):
     id = fields.IntField(pk=True)
     username = fields.CharField(max_length=50, unique=True)
-    hashed_password = fields.CharField(null=True, max_length=255)
-    is_active = fields.BooleanField(null=False, default=False)
-    confirmation = fields.UUIDField(null=True)
+    hashed_password = fields.CharField(max_length=255)
+    is_active = fields.BooleanField(default=False)
+    confirmation = fields.UUIDField(default=uuid4)
 
     class Meta:
         table: str = "user"
