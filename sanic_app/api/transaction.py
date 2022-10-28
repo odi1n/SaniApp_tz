415497ed408dcdd48b6ebf98dd3cb7a549ef4790
odi1n <sashf1999@mail.ru>
2022-10-28 03:29:32 +0300

    Поправил получение всех transaction

--- sanic_app/api/transaction.py
+++ sanic_app/api/transaction.py
@@ -1,4 +1,4 @@
-from sanic import Blueprint, HTTPResponse
+from sanic import Blueprint, json, HTTPResponse
 from sanic_ext.extensions.openapi import openapi
 from sanic_jwt import protected, inject_user
 
@@ -14,5 +14,5 @@ transaction = Blueprint("transaction", url_prefix="/transaction", strict_slashes
 @protected()
 @inject_user()
 async def get_transactions(request, user: User):
-    transaction = await TransactionQueryCreate.from_queryset(Transaction.filter(score__user_id=user.get('id')).all())
-    return HTTPResponse(body=transaction.json())
+    transaction = await TransactionQueryCreate.from_queryset(Transaction.filter(bill__user_id=user.get('id')).all())
+    return HTTPResponse(body=transaction.json(), content_type="application/json")
