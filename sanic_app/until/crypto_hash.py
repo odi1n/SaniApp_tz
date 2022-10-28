15e7c28c30e897584ee67bfd6a18cd1c7a2be42a
odi1n <sashf1999@mail.ru>
2022-10-29 00:05:59 +0300

    Сделал функцию хеширования

--- sanic_app/until/crypto_hash.py
+++ sanic_app/until/crypto_hash.py
@@ -2,5 +2,5 @@ from Crypto.Hash import SHA1
 
 
 def decrypto(private_key, transaction_id, user_id, bill_id, amount):
-    signature = SHA1.new(f'{private_key}:{transaction_id}:{user_id}:{bill_id}:{amount}'.encode()).hexdigest()
-    return signature
+    signature = SHA1.new(f'{private_key}:{transaction_id}:{user_id}:{bill_id}:{amount}'.encode())
+    return signature.hexdigest()
