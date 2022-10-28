f437a054af8aa927aa181bc8b0d208a2be357196
odi1n <sashf1999@mail.ru>
2022-10-28 03:29:08 +0300

    Поправил параметры transaction

--- sanic_app/serializers/params/transaction_params.py
+++ sanic_app/serializers/params/transaction_params.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel, PositiveInt, Field
 class TransactionParams(BaseModel):
     bill_id: PositiveInt = Field(description="Bill id")
     product_id: PositiveInt = Field(description="Product id")
-    amount: PositiveInt = Field(description="Amount")
 
     class Config:
         orm_mode = True
